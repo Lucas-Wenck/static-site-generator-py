dcd2553fd646550b6f690d15a2b1b7929358b69b
Lucas-Wenck <lucas.wenck@gmail.com>
2024-10-30 17:57:08 -0300

    add test cased for htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,6 +2,20 @@ import unittest
 
 from htmlnode import HTMLNode
 
-class TestHTMLNode:
-    def prop_to_html(self):
-        pass
+class TestHTMLNode(unittest.TestCase):
+    def test_prop_to_html(self):
+        test = HTMLNode("p", "loren ipsum", props={"href": "https://www.google.com", "target": "_blank",})
+        test_expected = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(test.props_to_html(), test_expected)
+
+    def test_prop_def_value(self):
+        test = HTMLNode("p", "loren ipsum",)
+        self.assertIs(test.props, None)
+
+    def test_node_repr(self):
+        test = HTMLNode("p", "loren ipsum", props={"href": "https://www.google.com", "target": "",})
+        test_expected = f"HTMLNode({test.tag}, {test.value}, {test.children}, {test.props})"
+        self.assertEqual(test.__repr__(), test_expected)
+
+if __name__ == "__main__":
+    unittest.main()
