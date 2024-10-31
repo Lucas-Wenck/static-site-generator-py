4edf96118ba8984f35132833d31daa4bddaca750
Lucas-Wenck <lucas.wenck@gmail.com>
2024-10-30 23:16:00 -0300

    feat: add LeafNode to htmlnode and add test cases

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_prop_to_html(self):
@@ -16,6 +16,14 @@ class TestHTMLNode(unittest.TestCase):
         test = HTMLNode("p", "loren ipsum", props={"href": "https://www.google.com", "target": "",})
         test_expected = f"HTMLNode({test.tag}, {test.value}, {test.children}, {test.props})"
         self.assertEqual(test.__repr__(), test_expected)
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
