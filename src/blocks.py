1fd26b09055159001d08d5c5cd98ee4e48c519b1
Lucas-Wenck <lucas.wenck@gmail.com>
2024-11-01 19:23:43 -0300

    feat: add function to divide blocks in types

--- src/blocks.py
+++ src/blocks.py
@@ -6,3 +6,32 @@ def markdown_to_blocks(markdown):
             clean_block.append(block[i].strip())
     return clean_block
     
+def block_to_block(markdown_block):
+    lines = markdown_block.split("\n")
+    if markdown_block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return "heading"
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return "code"
+    if markdown_block[0] == ">":
+        for line in lines:
+            if not line.startswith(">"):
+                return "paragraph"
+        return "quote"
+    if markdown_block[0:2] == "* ":
+        for line in lines:
+            if not line.startswith("* "):
+                return "paragraph"
+        return "unordered_list"
+    if markdown_block[0:2] == "- ":
+        for line in lines:
+            if not line.startswith("- "):
+                return "paragraph"
+        return "unordered_list"
+    if markdown_block[0:3] == "1. ":
+        count = 1
+        for line in lines:
+            if not line.startswith(f"{count}. "):
+                return "paragraph"
+            count += 1
+        return "ordered_list"
+    return "paragraph"
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -46,6 +46,19 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block(block), "heading")
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block(block), "code")
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block(block), "quote")
+        block = "* list\n* items"
+        self.assertEqual(block_to_block(block), "unordered_list")
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block(block), "ordered_list")
+        block = "paragraph"
+        self.assertEqual(block_to_block(block), "paragraph")
 
 if __name__ == "__main__":
     unittest.main()
