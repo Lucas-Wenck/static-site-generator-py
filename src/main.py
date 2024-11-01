cf84a524d5dc87d5b2e5c51150e70cf1b705550d
Lucas-Wenck <lucas.wenck@gmail.com>
2024-10-31 22:29:00 -0300

    feat: add method to convert textnode to htmlnode

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import *
 
 def main():
-    textnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    textnode = TextNode("This is a text node", "bold")
 
     print(textnode)
 
